dff6496fe4b5b880d0db99a002579b0ffb3304c9
jott_zeh <jan_christoph@gmx.net>
2022-05-20 00:01:54 +0200

    Define C1Token for c1 lexer

--- src/lexer/c1.rs
+++ src/lexer/c1.rs
@@ -3,9 +3,95 @@ use logos::Logos;
 #[derive(Logos, Debug, PartialEq)]
 pub enum C1Token {
     // TODO: Define variants and their token/regex
+    #[token("bool")]
+    KwBoolean, 
+
+    #[token("return")]
+    KwReturn,
+
+    #[token("if")]
+    KwIf,
+
+    #[token("else")]
+    KwElse,
+
+    #[token("float")]
+    KwFloat,
+
+    #[token("=")]
+    Assign,
+
+    #[token(";")]
+    Semicolon,
+
+    #[token("(")]
+    LParen,
+
+    #[token(")")]
+    RParen,
+
+    #[token("{")]
+    LBrace,
+
+    #[token("}")]
+    RBrace,
+
+    #[token(",")]
+    Comma,
+
+    #[token("||")]
+    Or,
+
+    #[token("==")]
+    Eq,
+
+    #[token("!=")]
+    Neq,
+
+    #[token("do")]
+    KwDo,
+
+    #[token("+")]
+    Plus,
+
+    #[token("&&")]
+    And,
+
+    #[token("*")]
+    Asterisk,
+    
+    #[token("void")]
+    KwVoid,
+
+    #[token("int")]
+    KwInt,
+
+    #[token("printf")]
+    KwPrintf,
+
+    #[token("while")]
+    KwWhile,
+
+    #[regex(r"[a-z][a-zA-Z_0-9]*")]
+    Id,
+
+    #[regex("[0-9]*")]
+    ConstInt,
+
+    #[regex("\"[^\"]*\"")]
+    ConstString,
+
+    #[regex(r"[0-9]*(\.|e)[0-9]*")]
+    ConstFloat,
+
+    #[regex("true|false")]
+    ConstBoolean,
 
     // Logos requires one token variant to handle errors,
     // it can be named anything you wish.
+    #[regex(r"/\* [^\*/]*\*/", logos::skip)]
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    #[regex(r"//[^\n]*", logos::skip)]
     #[error]
     Error,
 }
